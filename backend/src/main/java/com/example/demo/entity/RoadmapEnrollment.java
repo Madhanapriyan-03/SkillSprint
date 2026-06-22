a76baabd4b3ea6392fe0a23b45ed0eade047ff44
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 10:50:10 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
+++ backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roadmap_enrollment")
+
 public class RoadmapEnrollment {
-    
+    private Long id;
+        
 }
