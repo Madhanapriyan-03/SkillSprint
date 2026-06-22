2978ded3ea059eb59707ecfc37651c91b664322c
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 16:38:18 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/RoadmapMilestone.java
+++ backend/src/main/java/com/example/demo/entity/RoadmapMilestone.java
@@ -20,7 +20,50 @@ public class RoadmapMilestone {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String description;
-    private 
+    private Integer orderIndex;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getRoadmapId() {
+        return roadmapId;
+    }
+    public void setRoadmapId(Long roadmapId) {
+        this.roadmapId = roadmapId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getOrderIndex() {
+        return orderIndex;
+    }
+    public void setOrderIndex(Integer orderIndex) {
+        this.orderIndex = orderIndex;
+    }
+    
+    public RoadmapMilestone(Long id, Long roadmapId, String title, String description, Integer orderIndex) {
+        this.id = id;
+        this.roadmapId = roadmapId;
+        this.title = title;
+        this.description = description;
+        this.orderIndex = orderIndex;
+    }
+
+    public RoadmapMilestone() {
+    }
+    
+    
 
 
 }
