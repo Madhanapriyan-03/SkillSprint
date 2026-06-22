7a4d28fad2da51c333c751922b27588f52f3aa4b
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 10:47:10 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
+++ backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
@@ -3,17 +3,23 @@ package com.example.demo.entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "milestone_submission")
 
 public class MilestoneSubmission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long enrollment_id;
     private Long milestone_id;
+    private String contentUrl;
     private String status;
-    private Integer progressPercentage;
+    private Integer score;
     private LocalDateTime submittedAt;
 
 }
