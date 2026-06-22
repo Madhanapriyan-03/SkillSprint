446eb6558f06f8729241c8f6533fb7065a56b5c7
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 10:51:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
+++ backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
@@ -1,12 +1,21 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roadmap_enrollment")
 
 public class RoadmapEnrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-        
+    private Long student_id;
+    private Long roadmap_id;
+    private String status;
+    private Integer progressPer
+
 }
