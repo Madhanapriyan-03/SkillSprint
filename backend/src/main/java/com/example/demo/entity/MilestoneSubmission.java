b485f6ce8c2e72e24353762421456e0dd5e05a99
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 10:48:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
+++ backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
@@ -22,4 +22,58 @@ public class MilestoneSubmission {
     private Integer score;
     private LocalDateTime submittedAt;
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getEnrollment_id() {
+        return enrollment_id;
+    }
+    public void setEnrollment_id(Long enrollment_id) {
+        this.enrollment_id = enrollment_id;
+    }
+    public Long getMilestone_id() {
+        return milestone_id;
+    }
+    public void setMilestone_id(Long milestone_id) {
+        this.milestone_id = milestone_id;
+    }
+    public String getContentUrl() {
+        return contentUrl;
+    }
+    public void setContentUrl(String contentUrl) {
+        this.contentUrl = contentUrl;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Integer getScore() {
+        return score;
+    }
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+    public LocalDateTime getSubmittedAt() {
+        return submittedAt;
+    }
+    public void setSubmittedAt(LocalDateTime submittedAt) {
+        this.submittedAt = submittedAt;
+    }
+    
+    public MilestoneSubmission(Long id, Long enrollment_id, Long milestone_id, String contentUrl, String status,
+            Integer score, LocalDateTime submittedAt) {
+        this.id = id;
+        this.enrollment_id = enrollment_id;
+        this.milestone_id = milestone_id;
+        this.contentUrl = contentUrl;
+        this.status = status;
+        this.score = score;
+        this.submittedAt = submittedAt;
+    }
+
 }
