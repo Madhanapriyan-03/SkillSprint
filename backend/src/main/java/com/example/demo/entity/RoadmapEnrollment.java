e6a41897883af0f42d09d489d1fc00753023946c
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 15:50:18 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
+++ backend/src/main/java/com/example/demo/entity/RoadmapEnrollment.java
@@ -20,6 +20,51 @@ public class RoadmapEnrollment {
     private String status;
     private Integer progressPercentage;
     private LocalDateTime enrolledAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getStudent_id() {
+        return student_id;
+    }
+    public void setStudent_id(Long student_id) {
+        this.student_id = student_id;
+    }
+    public Long getRoadmap_id() {
+        return roadmap_id;
+    }
+    public void setRoadmap_id(Long roadmap_id) {
+        this.roadmap_id = roadmap_id;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Integer getProgressPercentage() {
+        return progressPercentage;
+    }
+    public void setProgressPercentage(Integer progressPercentage) {
+        this.progressPercentage = progressPercentage;
+    }
+    public LocalDateTime getEnrolledAt() {
+        return enrolledAt;
+    }
+    public void setEnrolledAt(LocalDateTime enrolledAt) {
+        this.enrolledAt = enrolledAt;
+    }
+    public RoadmapEnrollment(Long id, Long student_id, Long roadmap_id, String status, Integer progressPercentage,
+            LocalDateTime enrolledAt) {
+        this.id = id;
+        this.student_id = student_id;
+        this.roadmap_id = roadmap_id;
+        this.status = status;
+        this.progressPercentage = progressPercentage;
+        this.enrolledAt = enrolledAt;
+    }
 
 
 }
