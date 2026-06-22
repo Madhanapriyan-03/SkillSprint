4c4e4f69256d617ca9371d38a1314aaab56108be
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 17:46:27 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/LearningRoadmapRepository.java
+++ backend/src/main/java/com/example/demo/repository/LearningRoadmapRepository.java
@@ -1,5 +1,12 @@
 package com.example.demo.repository;
 
-public interface LearningRoadmapRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.LearningRoadmap;
+
+@Repository
+public interface LearningRoadmapRepository
+        extends JpaRepository<LearningRoadmap, Long> {
+
 }
