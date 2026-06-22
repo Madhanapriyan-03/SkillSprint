b5970d41c4a7e39867c73272c9884b30f0b7ae4a
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 16:44:18 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SprintAccount.java
+++ backend/src/main/java/com/example/demo/entity/SprintAccount.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class SprintAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Email
+    @Column(unique = true, nullable = false)
     private String email;
     private String passwordHash;
     private String role;
