9880b37529edb15888ac0250e52ad5a6135c8e23
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-22 10:02:31 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
+++ backend/src/main/java/com/example/demo/entity/MilestoneSubmission.java
@@ -1,5 +1,14 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "milestone_submission")
+
 public class MilestoneSubmission {
-    
+    private Long id;
+    private Long student_id;
+    private Long student_id;
+
 }
