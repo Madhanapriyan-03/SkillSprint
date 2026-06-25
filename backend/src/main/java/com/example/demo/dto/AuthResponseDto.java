d37175d285ebde21fe4e3d36fa00057ab587c2ec
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-25 12:26:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -1,5 +1,26 @@
 package com.example.demo.dto;
 
 public class AuthResponseDto {
-    private String token
+    private String token;
+    private String role;
+    private String email;
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
