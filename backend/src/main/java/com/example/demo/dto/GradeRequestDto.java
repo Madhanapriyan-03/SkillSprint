d2820c014c9b948005f574b12c5422197c5d73b8
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-28 10:17:33 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/GradeRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/GradeRequestDto.java
@@ -1,5 +1,21 @@
 package com.example.demo.dto;
 
 public class GradeRequestDto {
-    
+
+    private Integer score;
+
+    public GradeRequestDto() {
+    }
+
+    public GradeRequestDto(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
 }
