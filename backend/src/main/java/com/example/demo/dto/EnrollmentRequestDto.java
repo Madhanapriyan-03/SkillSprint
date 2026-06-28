0cc858c69bcc9f617608c3c37b7677c8e40d3a3c
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-28 10:16:03 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/EnrollmentRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/EnrollmentRequestDto.java
@@ -2,4 +2,19 @@ package com.example.demo.dto;
 
 public class EnrollmentRequestDto {
     private Long roadmapId;
+
+    public Long getRoadmapId() {
+        return roadmapId;
+    }
+    public void setRoadmapId(Long roadmapId) {
+        this.roadmapId = roadmapId;
+    }
+
+    public EnrollmentRequestDto(Long roadmapId) {
+        this.roadmapId = roadmapId;
+    }
+
+    public EnrollmentRequestDto() {
+    }
+    
 }
