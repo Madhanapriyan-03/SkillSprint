4f86cf0aa23d6e0a024ba16110843b14bb00fa94
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-28 09:53:33 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -1,33 +1,47 @@
 package com.example.demo.dto;
 
 public class AuthResponseDto {
-    private String token;
-    private String role;
+
+    private Long id;
     private String email;
-    public String getToken() {
-        return token;
+    private String role;
+    private String token;
+
+    public AuthResponseDto() {
     }
-    public void setToken(String token) {
+
+    public AuthResponseDto(Long id, String email, String role, String token) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
         this.token = token;
     }
-    public String getRole() {
-        return role;
+
+    public Long getId() {
+        return id;
     }
-    public void setRole(String role) {
-        this.role = role;
+    public void setId(Long id) {
+        this.id = id;
     }
+
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
-    public AuthResponseDto() {
+
+    public String getRole() {
+        return role;
     }
-    public AuthResponseDto(String token, String role, String email) {
-        this.token = token;
+    public void setRole(String role) {
         this.role = role;
-        this.email = email;
     }
-    
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
