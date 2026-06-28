afeba83423bd1e3a94d5ddfde31a5d879af72253
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-28 10:08:33 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -1,47 +1,33 @@
 package com.example.demo.dto;
 
 public class AuthResponseDto {
-
-    private Long id;
-    private String email;
-    private String role;
     private String token;
-
-    public AuthResponseDto() {
-    }
-
-    public AuthResponseDto(Long id, String email, String role, String token) {
-        this.id = id;
-        this.email = email;
-        this.role = role;
-        this.token = token;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-
+    private String role;
+    private String email;
     public String getToken() {
         return token;
     }
     public void setToken(String token) {
         this.token = token;
     }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public AuthResponseDto() {
+    }
+    public AuthResponseDto(String token, String role, String email) {
+        this.token = token;
+        this.role = role;
+        this.email = email;
+    }
+    
 }
