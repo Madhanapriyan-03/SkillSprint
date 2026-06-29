14b4387cdfedc4d542ce69da29f3b1029604f85e
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-29 14:16:07 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,20 +1,31 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.dto.AuthRequestDto;
 import com.example.demo.dto.AuthResponseDto;
 import com.example.demo.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    
-    @Autowired
-    AuthService aut;
 
-    @PostMapping("/create")
-    public AuthResponseDto 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<AuthResponseDto> register(@Valid @RequestBody AuthRequestDto dto) {
+        return new ResponseEntity<>(authService.register(dto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public AuthResponseDto login(@Valid @RequestBody AuthRequestDto dto) {
+        return authService.login(dto);
+    }
 }
