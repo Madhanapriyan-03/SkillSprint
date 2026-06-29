784f30040b63aa6336e7d5cfc9ee1eac47b58fb8
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-29 12:01:08 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.AuthService;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     
     @Autowired
-    
+    AuthService
 }
