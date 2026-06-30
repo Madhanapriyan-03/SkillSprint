c094e52760161ecc36916dae7f156964019b6628
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-06-30 16:16:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/LearningRoadmap.java
+++ backend/src/main/java/com/example/demo/entity/LearningRoadmap.java
@@ -78,7 +78,6 @@ public class LearningRoadmap {
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
