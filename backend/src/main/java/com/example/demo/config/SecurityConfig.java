7cdde49b841f1590baee35ca1425404b3b810ba7
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-07-05 17:39:23 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -38,7 +38,12 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers(
+                "/api/auth/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v3/api-docs/**"
+                ).permitAll()
                 
                 .anyRequest().authenticated()
             )
