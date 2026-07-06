5d524825fb9e4589810fc5c8879bc07bea1e6b32
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-07-06 08:29:39 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -7,18 +7,25 @@ public class AuthRequestDto {
     @Email
     @NotBlank
     private String email;
-
     @NotBlank
     private String password;
-
     private String role;
 
     public AuthRequestDto() {}
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail(){
+         return email; 
+    }
+    public void setEmail(String email) {
+         this.email = email; 
+    }
+
+    public String getPassword() { 
+        return password;
+    }
+    public void setPassword(String password) {
+         this.password = password; 
+    }
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
 }
