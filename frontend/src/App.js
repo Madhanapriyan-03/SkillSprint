58400bdc70fd38025e5ee0b6768ee576b2a31587
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-08-10 14:13:00 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import  App1  from './App1'
-export const App = () => {
+import App1 from './App1'
+const App = () => {
   return (
     <div>
       <App1/>
     </div>
   )
 }
+
+export default App
--- frontend/src/App1.jsx
+++ frontend/src/App1.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const App1 = () => {
+const App1 = () => {
   return (
     <div>
-        <h1>React app</h1>
+      <h1>React app</h1>
     </div>
   )
 }
+
+export default App1
