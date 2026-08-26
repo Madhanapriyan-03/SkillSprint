1b034100ed0bebbd0f6e9171b341a96d790b431f
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-08-26 13:45:36 +0530

    User commit

--- frontend/src/components/store/index.js
+++ frontend/src/components/store/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from '../../App';
-import reportWebVitals from '../../reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import roadmapReducer from './slices/roadmapSlice';
+import enrollmentReducer from './slices/enrollmentSlice';
+import submissionReducer from './slices/submissionSlice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    roadmaps: roadmapReducer,
+    enrollments: enrollmentReducer,
+    submissions: submissionReducer
+  },
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default store;
