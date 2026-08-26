1d2a5881478e537a85c60be5075eb8ecfe24f9ae
MADHANAPRIYAN M <727824TUAD118@skct.edu.in>
2026-08-26 13:47:06 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import App1 from './App1'
-const App = () => {
-  return (
-    <div>
-      <App1/>
-    </div>
-  )
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from './components/layout/Navbar';
+import Login from './components/Login';
+import ErrorHandler from './components/ErrorHandler';
+import NotificationStack from './components/NotificationStack';
+import Dashboard from './components/dashboard/Dashboard';
+import RoadmapList from './components/roadmap/RoadmapList';
+import EnrollmentList from './components/enrollment/EnrollmentList';
+
+import SubmissionList from './components/submission/SubmissionList';
+
+function PrivateRoute({ children }) {
+  const { user } = useSelector((state) => state.auth);
+  return user ? children : <Navigate to="/login" />;
 }
 
-export default App
+function App() {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <Router>
+      <div className="app">
+        {user && <Navbar />}
+        <ErrorHandler />
+        <NotificationStack />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/roadmaps" element={<PrivateRoute><RoadmapList /></PrivateRoute>} />
+          <Route path="/enrollments" element={<PrivateRoute><EnrollmentList /></PrivateRoute>} />
+          <Route path="/submissions" element={<PrivateRoute><SubmissionList /></PrivateRoute>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
